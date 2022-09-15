0df324387d0f7a5fcb5f823542457e5eb4da0ee0
Maksym Blinov <phoenix2160@gmail.com>
2022-09-15 20:00:49 +0200

    Initial commit

--- backup.py
+++ backup.py
@@ -1,18 +1,20 @@
 import os
 import sys
-import optparse
+import shutil
+import errno
 import yaml
 import time
 from datetime import datetime
 import dropbox
 from dropbox.files import WriteMode
 from dropbox.exceptions import ApiError, AuthError
+from tqdm import tqdm
+from pathlib import Path
 
 app_version = 0.1
 dir_path = os.path.dirname(os.path.abspath(__file__))
 timestamp_format = "%m/%d/%Y, %H:%M:%S"
-
-# Add OAuth2 access token here.
+# Add OAuth2 access token, by creating .token file
 # You can generate one for yourself in the App Console.
 # See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
 TOKEN = ''
@@ -29,32 +31,75 @@ def read_config():
     try:
         with open(f"{dir_path}/config.yaml", "r") as stream:
             try:
-                print(yaml.safe_load(stream))
+                return yaml.safe_load(stream)
             except yaml.YAMLError as exc:
                 log_error(exc)
     except FileNotFoundError as exc:
-        log_error(f"Backup config file not found - {exc}")
+        log_error(f"ERROR: Backup config file not found - {exc}")
 
 
 def retry(func, *func_args, **kwargs):
     count = kwargs.pop("count", 5)
     delay = kwargs.pop("delay", 5)
     return any(func(*func_args, **kwargs)
-               or log_error("waiting for %s seconds before retyring again" % delay)
+               or log_error("ERROR: Failed to upload to Dropbox - waiting for %s seconds before retyring again" % delay)
                or time.sleep(delay)
                for _ in range(count))
 
 
-class BackupConfig:
-    def __init__(self, backup_type, verify):
-        self.type = backup_type
-        self.verify = verify
+class Backup:
+    def __init__(self, backup_name, **kwargs):
+        self.backup_name = backup_name
+        for key, value in kwargs.items():
+            if key in ['backup_type', 'source', 'destination', 'auth']:
+                setattr(self, key, value)
+            else:
+                log_error(f"ERROR: Wrong configuration parameter: {key}")
+                sys.exit()
 
 
-def dropbox_backup(file, backup_path):
+def backup_dir(sources, destination):
+    for source in sources:
+        print(f"Backing up {source}")
+        for (dirpath, dirnames, filenames) in (os.walk(source)):
+            for filename in tqdm(filenames, desc=os.path.basename(dirpath), leave=False):
+                file_from = os.path.join(os.path.abspath(dirpath), filename)
+                if 'local' in destination:
+                    file_to = Path(destination['local']['path'], os.path.basename(source), filename)
+                    local_backup(file_from, file_to)
+
+                if 'dropbox' in destination:
+                    file_to = Path(destination['dropbox']['path'], os.path.basename(source), filename)
+                    retry(dropbox_upload, file_from, file_to.as_posix(), count=3, delay=1)
+
+
+def local_backup(file, backup_path):
+    try:
+        shutil.copy(file, backup_path)
+    except IOError as e:
+        # ENOENT(2): file does not exist, raised also on missing dest parent dir
+        if e.errno != errno.ENOENT:
+            raise
+        # try creating parent directories
+        os.makedirs(os.path.dirname(backup_path))
+        shutil.copy(file, backup_path)
+
+
+def dropbox_upload(file_from, file_to):
+    try:
+        with open(f"{dir_path}/.token") as t:
+            TOKEN = t.read()
+
+            if len(TOKEN) == 0:  # Check for access token
+                log_error("ERROR: Looks like you didn't add your Dropbox access token. ")
+                sys.exit()
+
+    except FileNotFoundError as err:
+        log_error(err)
+        sys.exit()
+
     # Create an instance of a Dropbox class, which can make requests to the API.
     with dropbox.Dropbox(TOKEN) as dbx:
-
         # Check that the access token is valid
         try:
             dbx.users_get_current_account()
@@ -64,28 +109,36 @@ def dropbox_backup(file, backup_path):
             log_error(msg)
             sys.exit(msg)
         # Create a backup
-        with open(file, 'rb') as f:
+        with open(file_from, 'rb') as f:
             # We use WriteMode=overwrite to make sure that the settings in the file
             # are changed on upload
-            print("Uploading " + file + " to Dropbox as " + backup_path + "...")
             try:
-                dbx.files_upload(f.read(), file, mode=WriteMode('overwrite'))
+                dbx.files_upload(f.read(), file_to, mode=WriteMode('overwrite'))
+                return True
             except ApiError as err:
                 # This checks for the specific error where a user doesn't have
                 # enough Dropbox space quota to upload this file
                 if (err.error.is_path() and
                         err.error.get_path().reason.is_insufficient_space()):
-                    sys.exit("ERROR: Cannot back up; insufficient space.")
+                    log_error("ERROR: Cannot back up to Dropbox; insufficient space.")
+                    sys.exit()
                 elif err.user_message_text:
                     print(err.user_message_text)
                     sys.exit()
                 else:
                     log_error(err)
-                    sys.exit()
+                    return False
 
 
 def main():
-    read_config()
+    config_dict = read_config()
+    for item in config_dict:
+        b = Backup(item, **config_dict[item])
+        try:
+            if b.backup_type == 'dir':
+                backup_dir(b.source, b.destination)
+        except AttributeError as err:
+            log_error(f"ERROR: wrong Backup config {err}")
 
 
 if __name__ == '__main__':
