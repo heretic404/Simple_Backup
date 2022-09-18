8497792d6933054e997a7f72908d2a61aa249471
Maksym Blinov <phoenix2160@gmail.com>
2022-09-18 17:31:55 +0200

    App refactoring and code optimization

--- backup.py
+++ backup.py
@@ -1,150 +1,203 @@
-import os
-import sys
-import shutil
 import errno
-import yaml
+import optparse
+import os
+import shutil
+import sys
 import time
-import zlib
 from datetime import datetime
-import dropbox
-from dropbox.files import WriteMode
-from dropbox.exceptions import ApiError, AuthError
-from tqdm import tqdm
 from pathlib import Path
 
-app_version = '0.1.1'
+import dropbox
+import yaml
+from dropbox.exceptions import ApiError, AuthError
+from dropbox.files import WriteMode
+from tqdm import tqdm
+
+app_version = '0.2.0'
 dir_path = os.path.dirname(os.path.abspath(__file__))
 timestamp_format = "%m/%d/%Y, %H:%M:%S"
-# Add OAuth2 access token, by creating .token file
-# You can generate one for yourself in the App Console.
-# See <https://blogs.dropbox.com/developers/2014/05/generate-an-access-token-for-your-own-account/>
-TOKEN = ''
+
+parser = optparse.OptionParser(
+    usage=f"""
+    This script will back up your directories to Dropbox or local directory 
+
+    usage: %prog [options]
+    example: python %prog -q
+    example: python %prog -q -c /foo/bar/config.yaml
+    """,
+    version=f"v{app_version}"
+)
+parser.add_option("-q", "--quiet",
+                  action="store_false", dest="verbose", default=True,
+                  help="don't print status messages to stdout")
+
+parser.add_option("-c", "--config",
+                  action="store",  # optional because action defaults to "store"
+                  dest="config",
+                  default=f"{dir_path}/config.yaml",
+                  help="OPTIONAL: Config file location", )
+
+parser.add_option("-t", "--token",
+                  action="store",  # optional because action defaults to "store"
+                  dest="token",
+                  default=f"{dir_path}/.token",
+                  help="OPTIONAL: Token file location", )
+
+options, args = parser.parse_args()
+option_dict = vars(options)
 
 
-def log_error(message):
-    with open(f"{dir_path}/error.log", "a", encoding='utf-8') as f:
-        timestamp = datetime.now().strftime(timestamp_format)
-        f.write(f"[{timestamp}]: {message}\n")
-    print(message)
-
-
-def read_config():
+def read_config(config_path):
     try:
-        with open(f"{dir_path}/config.yaml", "r") as stream:
+        with open(config_path, "r") as stream:
             try:
                 return yaml.safe_load(stream)
             except yaml.YAMLError as exc:
-                log_error(exc)
+                SimpleLogger.error(exc)
     except FileNotFoundError as exc:
-        log_error(f"ERROR: Backup config file not found - {exc}")
+        SimpleLogger.error(f"Backup config file not found - {exc}")
 
 
-def retry(func, *func_args, **kwargs):
-    count = kwargs.pop("count", 5)
-    delay = kwargs.pop("delay", 5)
-    return any(func(*func_args, **kwargs)
-               or log_error("ERROR: Failed to upload to Dropbox - waiting for %s seconds before retyring again" % delay)
-               or time.sleep(delay)
-               for _ in range(count))
+class SimpleLogger:
+    @staticmethod
+    def error(message):
+        with open(f"{dir_path}/error.log", "a", encoding='utf-8') as f:
+            timestamp = datetime.now().strftime(timestamp_format)
+            f.write(f"[{timestamp}]: ERROR: {message}\n")
+        sys.exit(f"ERROR: {message}")
+
+    @staticmethod
+    def msg(message):
+        with open(f"{dir_path}/backup.log", "a", encoding='utf-8') as f:
+            timestamp = datetime.now().strftime(timestamp_format)
+            f.write(f"[{timestamp}]: {message}\n")
+        if option_dict['verbose'] is True:
+            print(message)
 
 
 class Backup:
-    def __init__(self, backup_name, **kwargs):
+    def __init__(self, backup_name, backup_type, sources, destination):
         self.backup_name = backup_name
-        for key, value in kwargs.items():
-            if key in ['backup_type', 'source', 'destination', 'auth']:
-                setattr(self, key, value)
-            else:
-                log_error(f"ERROR: Wrong configuration parameter: {key}")
-                sys.exit()
+        self.backup_type = backup_type
+        self.sources = sources
+        self.destination = destination
+
+    def backup_dir(self):
+        for source in self.sources:
+            SimpleLogger.msg(f"Backing up {source}")
+            for (dirpath, dirnames, filenames) in (os.walk(source)):
+                for filename in tqdm(filenames, desc=os.path.basename(dirpath), leave=False):
+                    file_from = Path(dirpath, filename)
+                    if 'local' in self.destination:
+                        file_to = Path(self.destination['local']['path'],
+                                       Path(dirpath).relative_to(Path(source).parent),
+                                       filename)
+                        local_bak = LocalBackup(file_from, file_to)
+                        local_bak.copy_files()
+                    if 'dropbox' in self.destination:
+                        file_to = Path(self.destination['dropbox']['path'],
+                                       Path(dirpath).relative_to(Path(source).parent),
+                                       filename)
+                        dbx_bak = DropboxBackup(file_from, file_to.as_posix())
+                        dbx_bak.upload()
+        SimpleLogger.msg("Backup is complete.")
 
 
-def backup_dir(sources, destination):
-    for source in sources:
-        print(f"Backing up {source}")
-        for (dirpath, dirnames, filenames) in (os.walk(source)):
-            for filename in tqdm(filenames, desc=os.path.basename(dirpath), leave=False):
-                file_from = Path(dirpath, filename)
-                if 'local' in destination:
-                    file_to = Path(destination['local']['path'],
-                                   Path(dirpath).relative_to(Path(source).parent),
-                                   filename)
-                    local_backup(file_from, file_to)
+class LocalBackup:
+    def __init__(self, file, backup_path, archive=False):
+        self.file = file
+        self.backup_path = backup_path
+        self.archive = archive
 
-                if 'dropbox' in destination:
-                    file_to = Path(destination['dropbox']['path'],
-                                   Path(dirpath).relative_to(Path(source).parent),
-                                   filename)
-                    retry(dropbox_upload, file_from, file_to.as_posix(), count=3, delay=1)
-    print("Backup is complete.")
-
-
-def local_backup(file, backup_path):
-    try:
-        shutil.copy(file, backup_path)
-    except IOError as e:
-        # ENOENT(2): file does not exist, raised also on missing dest parent dir
-        if e.errno != errno.ENOENT:
-            raise
-        # try creating parent directories
-        os.makedirs(os.path.dirname(backup_path))
-        shutil.copy(file, backup_path)
-
-
-def dropbox_upload(file_from, file_to):
-    try:
-        with open(f"{dir_path}/.token") as t:
-            TOKEN = t.read()
-
-            if len(TOKEN) == 0:  # Check for access token
-                log_error("ERROR: Looks like you didn't add your Dropbox access token. ")
-                sys.exit()
-
-    except FileNotFoundError as err:
-        log_error(err)
-        sys.exit()
-
-    # Create an instance of a Dropbox class, which can make requests to the API.
-    with dropbox.Dropbox(TOKEN) as dbx:
-        # Check that the access token is valid
+    def copy_files(self):
         try:
-            dbx.users_get_current_account()
-        except AuthError:
-            msg = "ERROR: Invalid Dropbox access token; " \
-                  "try re-generating an access token from the app console on the web."
-            log_error(msg)
-            sys.exit(msg)
-        # Create a backup
-        with open(file_from, 'rb') as f:
-            # We use WriteMode=overwrite to make sure that the settings in the file
-            # are changed on upload
+            shutil.copy(self.file, self.backup_path)
+        except IOError as e:
+            # ENOENT(2): file does not exist, raised also on missing dest parent dir
+            if e.errno != errno.ENOENT:
+                raise
+            # try creating parent directories
+            os.makedirs(os.path.dirname(self.backup_path))
+            shutil.copy(self.file, self.backup_path)
+
+
+class DropboxBackup:
+    def __init__(self, file_from, file_to):
+        self.file_from = file_from
+        self.file_to = file_to
+        self.token = self.read_token()
+
+    @staticmethod
+    def read_token():
+        try:
+            with open(option_dict['token']) as t:
+                token = t.read()
+                if len(token) == 0:  # Check for access token
+                    SimpleLogger.error("Looks like you didn't add your Dropbox access token. ")
+                return token
+        except FileNotFoundError as err:
+            SimpleLogger.error(err)
+
+    @staticmethod
+    def retry(func, *func_args, **kwargs):
+        count = kwargs.pop("count", 5)
+        delay = kwargs.pop("delay", 5)
+        return any(func(*func_args, **kwargs)
+                   or SimpleLogger.error(f"Failed to upload to Dropbox - "
+                                         f"waiting for {delay} seconds before re-tyring again")
+                   or time.sleep(delay)
+                   for _ in range(count))
+
+    def upload(self):
+        # Create an instance of a Dropbox class, which can make requests to the API.
+        with dropbox.Dropbox(self.token) as dbx:
+            # Check that the access token is valid
             try:
-                dbx.files_upload(f.read(), file_to, mode=WriteMode('overwrite'))
-                return True
-            except ApiError as err:
-                # This checks for the specific error where a user doesn't have
-                # enough Dropbox space quota to upload this file
-                if (err.error.is_path() and
-                        err.error.get_path().reason.is_insufficient_space()):
-                    log_error("ERROR: Cannot back up to Dropbox; insufficient space.")
-                    sys.exit()
-                elif err.user_message_text:
-                    print(err.user_message_text)
-                    sys.exit()
-                else:
-                    log_error(err)
-                    return False
+                dbx.users_get_current_account()
+            except AuthError:
+                msg = "Invalid Dropbox access token; " \
+                      "try re-generating an access token from the app console on the web."
+                SimpleLogger.error(msg)
+            # Create a backup
+            with open(self.file_from, 'rb') as f:
+                # We use WriteMode=overwrite to make sure that the settings in the file
+                # are changed on upload
+                try:
+                    dbx.files_upload(f.read(), self.file_to, mode=WriteMode('overwrite'))
+                    return True
+                except ApiError as err:
+                    # This checks for the specific error where a user doesn't have
+                    # enough Dropbox space quota to upload this file
+                    if (err.error.is_path() and
+                            err.error.get_path().reason.is_insufficient_space()):
+                        SimpleLogger.error("Cannot back up to Dropbox; insufficient space.")
+                    elif err.user_message_text:
+                        SimpleLogger.error(err.user_message_text)
+                    else:
+                        SimpleLogger.error(err)
+                        return False
 
 
 def main():
-    config_dict = read_config()
+    config_dict = read_config(option_dict['config'])
     for item in config_dict:
-        b = Backup(item, **config_dict[item])
+        i = config_dict[item]
+        for key, value in i.items():
+            if key not in ['backup_type', 'source', 'destination']:
+                SimpleLogger.error(f"Wrong configuration parameter: {key}")
         try:
+            b = Backup(item, i['backup_type'], i['source'], i['destination'])
             if b.backup_type == 'dir':
-                backup_dir(b.source, b.destination)
+                b.backup_dir()
+            elif b.backup_type == 'db':
+                print(i)
+                SimpleLogger.msg("WARNING: Database backups not supported yet")
+            else:
+                SimpleLogger.error(f"unsupported backup type: {b.backup_type}")
+        except KeyError as err:
+            SimpleLogger.error(f"wrong Backup config, missing {err} parameter")
         except AttributeError as err:
-            log_error(f"ERROR: wrong Backup config {err}")
+            SimpleLogger.error(f"wrong Backup config, {err}")
         except KeyboardInterrupt:
             print("Operation is aborted by user")
             sys.exit(0)
